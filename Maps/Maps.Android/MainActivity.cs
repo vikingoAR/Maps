5b8dcb473d1fccc5565aec5efd7709860d8f3014
SergioCossa <chech@DESKTOP-EMVGOQS>
2018-10-12 12:53:13 -0300

    First changes

--- Maps/Maps.Android/MainActivity.cs
+++ Maps/Maps.Android/MainActivity.cs
@@ -1,11 +1,7 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace Maps.Droid
 {
@@ -19,7 +15,15 @@ namespace Maps.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- Maps/Maps/MainPage.xaml.cs
+++ Maps/Maps/MainPage.xaml.cs
@@ -1,17 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace Maps
 {
     public partial class MainPage : ContentPage
     {
+        Position mapPosition;
+        Location userLocation;
+
         public MainPage()
         {
             InitializeComponent();
+            FindUserLocation();
+
+            mapPosition = new Position(userLocation.Latitude, userLocation.Longitude);
+
+            MapView.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    mapPosition, Distance.FromMiles(1)));
+        }
+
+        async Task FindUserLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                userLocation = await Geolocation.GetLocationAsync(request);
+                //userLocation = await Geolocation.GetLastKnownLocationAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
         }
     }
 }
